1a348f9512c418001b53456d5bbe0228d5f76ace
NourFagou <nourfagou@gmail.com>
2023-04-09 00:58:02 -0700

    updated

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,10 +1,10 @@
 #include "main.h"
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
-#include <string.h>
 
-void _puts(char *s)
+int _putchar(char c)
 {
-    puts(s);
+	return putchar(c);
 }
